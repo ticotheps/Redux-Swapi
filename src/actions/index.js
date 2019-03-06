f256097654d661c5089073ea8664730319ae088c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-06 17:06:50 -0500

    Renamed action type FETCH_CHAR_START to action type FETCH_CHAR_FETCHING and created switch statements for each action type

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
-export const FETCH_CHAR_START = "FETCH_CHAR_START";
+export const FETCH_CHAR_FETCHING = "FETCH_CHAR_FETCHING";
 export const FETCH_CHAR_SUCCESS = "FETCH_CHAR_SUCCESS";
 export const FETCH_CHAR_FAILURE = "FETCH_CHAR_FAILURE";
 
@@ -11,7 +11,7 @@ export const FETCH_CHAR_FAILURE = "FETCH_CHAR_FAILURE";
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have control over our thunk-based action creator
 export const getChar = () => dispatch => {
-    dispatch({ type: FETCH_CHAR_START });
+    dispatch({ type: FETCH_CHAR_FETCHING });
 
     axios
         .get("https://swapi.co/api/people/")
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,5 +1,5 @@
 import {
-  FETCH_CHAR_START,
+  FETCH_CHAR_FETCHING,
   FETCH_CHAR_SUCCESS,
   FETCH_CHAR_FAILURE
 } from "../actions";
@@ -7,15 +7,35 @@ import {
 
 const initialState = {
   characters: [], 
-  fetching: false,
+  isFetching: false,
   error: ''
   // Array characters, Boolean fetching, null error.
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCH_CHAR_FETCHING:
+      return {
+        ...state,
+        error: '',
+        isFetching: true
+      };
+    case FETCH_CHAR_SUCCESS:
+      return {
+        ...state,
+        error: '',
+        isFetching: false,
+        characters: action.payload
+      };
+    case FETCH_CHAR_FAILURE:
+      return {
+        ...state,
+        error: "Whoops, something went wrong.",
+        isFetching: false
+      };
     default:
       return state;
   }
