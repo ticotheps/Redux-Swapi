60aa6ec071942b40d7ac97c35e0f0cfd714af388
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-06 16:21:50 -0500

    Created an action type for the starting process of fetching the Star Wars characters from the API

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
 
+export const FETCH_CHAR_START = "FETCH_CHAR_START";
+
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
