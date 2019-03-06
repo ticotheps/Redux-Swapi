c3150f1e9c8700662a382a6875594ecdc0b01fb5
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-06 16:28:46 -0500

    Created axios API GET() request inside action creator 'getChar()' function in actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,9 +3,20 @@ import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
-
 export const FETCH_CHAR_START = "FETCH_CHAR_START";
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+export const getChar = () => dispatch => {
+    dispatch({ type: FETCH_CHAR_START });
+
+    axios
+        .get("https://swapi.co/api/people/")
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
